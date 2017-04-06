42799da0bc1fde568d93b6e65708eb87ec4c1c67
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-04-07 01:12:10 +0200

    - working on the first word count implementation

--- examples/wordCount.js
+++ examples/wordCount.js
@@ -1,2 +1,34 @@
 "use strict";
 
+//# https://github.com/apache/kafka/blob/0.10.0/streams/examples/src/main/java/org/apache/kafka/streams/examples/wordcount/WordCountDemo.java
+
+const {KStream, KafkaStreams} = require("./../index.js");
+const config = require("./../test/test-config.js");
+
+function etl_ValueFlatten(value){
+    return value.toLowerCase().split(" ");
+}
+
+function etl_KeyValueMapper(key, value){
+    return {
+        key,
+        value
+    };
+}
+
+const source = new KStream("streams-file-input");
+
+//KTable
+const counts = source
+    .flatMapValues(etl_ValueFlatten)
+        .map(etl_KeyValueMapper)
+        .countByKey("Counts");
+
+counts.to("streams-wordcount-output");
+
+const streams = new KafkaStreams([source], config);
+streams.start();
+
+setTimeout(() => {
+    streams.close();
+}, 5000);
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 "use strict";
 
-module.exports = {
+const KStream = require("./lib/KStream.js");
+const KTable = require("./lib/KTable.js");
+const KafkaFactory = require("./lib/KafkaFactory.js");
+const KafkaStreams = require("./lib/KafkaStreams.js");
 
+module.exports = {
+    KStream,
+    KTable,
+    KafkaFactory,
+    KafkaStreams
 };
