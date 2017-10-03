ed9cb14e55c8499f3e06e913c6e490e9830a3417
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-10-03 14:16:27 +0200

    longer window for travis

--- test/int/Streams.test.js
+++ test/int/Streams.test.js
@@ -315,7 +315,7 @@ describe("Streams Integration", function() {
     });
 
     it("should be able to abort a running window", function(done){
-        this.timeout(5000);
+        this.timeout(10000);
 
         const inputStream = kafkaStreams.getKStream(null);
 
